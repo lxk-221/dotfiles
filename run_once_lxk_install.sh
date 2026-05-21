483e96a65809c62f0d80988c87bab43904fa40d7
lxk <376721246@qq.com>
2026-05-21 20:53:10 +0800

    fix

--- run_once_lxk_install.sh
+++ run_once_lxk_install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-# zsh install
-sudo apt install zsh -y
+echo "[1/5] Installing zsh..."
+command -v zsh &>/dev/null || sudo apt install zsh -y
 
-# omz install
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+echo "[2/5] Installing oh-my-zsh..."
+[ -d "$HOME/.oh-my-zsh" ] || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
-# omz plugins
+echo "[3/5] Installing zsh plugins..."
 dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -d "$dir" ] || git clone https://github.com/zsh-users/zsh-autosuggestions "$dir"
 dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 [ -d "$dir" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$dir"
 command -v autojump &>/dev/null || sudo apt-get install -y autojump
 
-# starship
-curl -sS https://starship.rs/install.sh | sh
+echo "[4/5] Installing starship..."
+command -v starship &>/dev/null || curl -sS https://starship.rs/install.sh | sh
+
+echo "[5/5] Done!"
