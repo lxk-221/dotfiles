dac243a5e11071dc2a5c02775f3ca426b5b1dd42
lxk <376721246@qq.com>
2026-05-21 21:37:44 +0800

    add change default shell command

--- run_once_lxk_install.sh
+++ run_once_lxk_install.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 set -e
 
-echo "[1/5] Installing zsh..."
+echo "[1/6] Installing zsh..."
 command -v zsh &>/dev/null || sudo apt install zsh -y
 
-echo "[2/5] Installing oh-my-zsh..."
+echo "[2/6] Installing oh-my-zsh..."
 [ -d "$HOME/.oh-my-zsh" ] || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
-echo "[3/5] Installing zsh plugins..."
+echo "[3/6] Installing zsh plugins..."
 dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -d "$dir" ] || git clone https://github.com/zsh-users/zsh-autosuggestions "$dir"
 dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 [ -d "$dir" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$dir"
 command -v autojump &>/dev/null || sudo apt-get install -y autojump
 
-echo "[4/5] Installing starship..."
+echo "[4/6] Installing starship..."
 if ! command -v starship &>/dev/null; then
-    curl -sS https://starship.rs/install.sh | sh -s -- -y
+    curl -sSL https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz | sudo tar xz -C /usr/local/bin
 fi
 
-echo "[5/5] Done!"
+echo "[5/6] Setting default shell to zsh..."
+if [[ "$SHELL" != *zsh* ]]; then
+    chsh -s "$(command -v zsh)"
+fi
+
+echo "[6/6] Done! Please log out and log back in, then restart tmux (tmux kill-server) for changes to take effect."
